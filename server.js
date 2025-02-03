66d6c97da9e4146426c71b91dbb4703a7627dafd
ranaabdellatif <ranaabdellatif645@gmail.com>
2025-02-03 16:42:24 -0700

    updates

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const app = express();
-//const port = 3000; // You can choose any port you like
 
 app.use(express.json()); // To parse JSON bodies
 
 app.post('/', (req, res) => {
-  const acceptHeader = req.headers['accept'];
+    const acceptHeader = req.get['Accept'];
+
+    if (acceptHeader){
     res.setHeader('accept', acceptHeader);
+    }
 
     const responseBody = {
-      acceptheader: acceptHeader, 
       ...req.body
     }
+
     res.json(responseBody)
     });
 
     const port = process.env.PORT || 3000;
     const server = app.listen(port, () => {
-      console.log('Server is running on port ${port}');
+      console.log(`Server is running on port ${port}`);
     })
 
   module.exports = server;
--- test/echotest.js
+++ test/echotest.js
@@ -1,14 +1,14 @@
 //Require the dev-dependencies
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../app');
+const server = require('../server');
 const {expect} = chai;
 
 chai.use(chaiHttp);
 
-describe('POST', () => {
+describe('POST /', () => {
 
-    it('should return a JSOn object with the accept header', (done) => {
+    it('should return a JSON object with the accept header', (done) => {
         const sampleBody = { message: "Hello, World!"};
 
         chai 
@@ -22,7 +22,7 @@ describe('POST', () => {
                 expect(response).to.be.json;
                 expect(response.body).to.have.property('message', "Hello, World!")
                 done();
-            })
+            });
 
             });
 
